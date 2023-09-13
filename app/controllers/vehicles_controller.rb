0c5b115f3be882004ff407ad6980ebb6539b13e5
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-12 22:00:21 -0300

    style: lint with rubocop

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class VehiclesController < ApplicationController
   def index
     @vehicles = Vehicle.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :vehicles, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :dealerships, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :vehicles, only: %i[index new create edit update destroy]
+  resources :dealerships, only: %i[index new create edit update destroy]
 end
