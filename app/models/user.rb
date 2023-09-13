815113470fecd65aec8e3dabee6baa7f64b9689c
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-13 13:08:08 -0300

    feat: create user model and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# frozen-string-literal: true
+
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :role, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true, confirmation: true
+  enum :role, admin: 'admin', dealership: 'dealership'
+  validate :dealership_presence, if: :dealership?
+
+  belongs_to :dealership, optional: true
+
+  devise :database_authenticatable, :registerable, :validatable
+
+  def admin?
+    role == 'admin' # :admin?
+  end
+
+  def dealership?
+    role == 'dealership'
+  end
+
+  private
+
+  def dealership_presence
+    if dealership.blank?
+      errors.add(:base, 'must be associated to a valid dealership')
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_190319) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_011332) do
   create_table "dealerships", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "role", null: false
+    t.integer "dealership_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dealership_id"], name: "index_users_on_dealership_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vehicles", force: :cascade do |t|
@@ -29,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_190319) do
     t.index ["dealership_id"], name: "index_vehicles_on_dealership_id"
   end
 
+  add_foreign_key "users", "dealerships"
   add_foreign_key "vehicles", "dealerships"
 end
