fe91e51c651e5c68f22a35458a9db44c729c59da
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-14 18:44:52 -0300

    refactor: update users controller with error rescuing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,18 +11,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     @user.save!
+
     redirect_to @user, status: :created
-  rescue ActiveRecord::RecordInvalid
-    render :new, status: :unprocessable_entity
   end
 
   def show
     @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert] = 'User Not Found'
-    render status: :not_found
   end
 
   private
