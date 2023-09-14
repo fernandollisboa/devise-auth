e86676e40444567d510d52e009f622ada00d371b
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-14 18:44:52 -0300

    refactor: update users controller with error rescuing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    redirect_to root_path, status: :not_found
+  end
+
+  def unprocessable_entity
+    render :new, status: :unprocessable_entity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'vehicles#index'
   resources :users
   devise_for :users
   resources :vehicles, only: %i[index new create edit update destroy]
