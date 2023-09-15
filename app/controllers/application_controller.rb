15993b7ccf9325de1f939cadee94b4ddb983af4b
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2023-09-15 12:13:48 -0300

    feat: add authorization after_action in appplication controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
 
@@ -12,5 +18,10 @@ class ApplicationController < ActionController::Base
 
   def unprocessable_entity
     render :new, status: :unprocessable_entity
+  end
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_back(fallback_location: root_path)
   end
 end
